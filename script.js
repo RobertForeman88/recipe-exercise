46ee5ffbe42f65a24df65a98fe27cb7419f033dc
Bobby Foreman <bobbyforeman@Bobbys-MacBook-Pro.local>
2019-06-22 10:29:51 -0500

    completed recipe-exercise

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var two = 2;
+var oneForth = '1/4';
+
+function marinaraSauce(cookName){
+    console.log('Hey, ' + cookName + '! This is my marinaraSauce recipe!' )
+    console.log('Heat a medium-large sauce pan over medium heat');
+    console.log('Add' + two +  ' tspns of oil and 5 garlic cloves, cook until golden brown, about ' + two +  ' minutes');
+    console.log('Add' + oneForth + ' cup Water, ' + two + ' cans of crush tomates, and salt, and season with black pepper to taste');
+    console.log('Cover pot, Bring to a boil, reduce the heat to medium-low, and simmer until the sauceis heated, about 10 minute');
+    console.log('Stir in ' + oneForth + ' cup of roughly chopped basil, salt and pepper as needed');
+
+
+
+}
+
+marinaraSauce('Adam'); 
+
